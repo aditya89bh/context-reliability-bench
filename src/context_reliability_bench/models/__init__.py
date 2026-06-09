f6f34f65d171b7c803ad35e6bf33713ca5c16880
Aditya <aditya89bh@gmail.com>
2026-06-10 02:49:00 +0530

    Add benchmark run result model

--- src/context_reliability_bench/models/__init__.py
+++ src/context_reliability_bench/models/__init__.py
@@ -3,5 +3,13 @@ from context_reliability_bench.models.document import Document
 from context_reliability_bench.models.metric_result import MetricResult
 from context_reliability_bench.models.query import Query
 from context_reliability_bench.models.retrieved_context import RetrievedContext
+from context_reliability_bench.models.run_result import RunResult
 
-__all__ = ["BenchmarkCase", "Document", "MetricResult", "Query", "RetrievedContext"]
+__all__ = [
+    "BenchmarkCase",
+    "Document",
+    "MetricResult",
+    "Query",
+    "RetrievedContext",
+    "RunResult",
+]
---
+++ src/context_reliability_bench/models/run_result.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from context_reliability_bench.models.metric_result import MetricResult
+
+
+@dataclass(frozen=True)
+class RunResult:
+    run_id: str
+    metric_results: tuple[MetricResult, ...]
